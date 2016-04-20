6c6112e60f2aeae27997f66b7e6ab67ca47406ba
Yannick Valy <yannick.valy@gmail.com>
2016-04-20 13:26:38 +0200

    Using Material Design

--- ChronoTalk/ChronoTalk.Droid/MainActivity.cs
+++ ChronoTalk/ChronoTalk.Droid/MainActivity.cs
@@ -1,26 +1,20 @@
-﻿using System;
-
-using Android.App;
+﻿using Android.App;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
 
 namespace ChronoTalk.Droid
 {
     [Activity(Label = "MainActivity", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    //public class MainActivity : Xamarin.Forms.Platform.Android.FormsAppCompatActivity
-    public class MainActivity : Xamarin.Forms.Platform.Android.FormsApplicationActivity
+    public class MainActivity : FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
         {
-            //ToolbarResource = Resource.Layout.toolbar;
-            //TabLayoutResource = Resource.Layout.tabs;
-            Xamarin.Forms.Forms.Init(this, bundle);
-
             base.OnCreate(bundle);
-
+            Forms.Init(this, bundle);
+            ToolbarResource = Resource.Layout.toolbar;
+            TabLayoutResource = Resource.Layout.tabs;
             LoadApplication(new App());
         }
     }
