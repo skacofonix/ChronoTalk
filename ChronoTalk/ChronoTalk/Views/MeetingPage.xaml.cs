d3cba9af8a03cbe3b3353c4a8bd0b595ddf28fb7
Yannick Valy <yannick.valy@gmail.com>
2016-04-29 13:31:51 +0200

    Symplift MeetingPage code behind

--- ChronoTalk/ChronoTalk/Views/MeetingPage.xaml.cs
+++ ChronoTalk/ChronoTalk/Views/MeetingPage.xaml.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Threading.Tasks;
-using ChronoTalk.ViewModels;
+﻿using ChronoTalk.ViewModels;
 using Xamarin.Forms;
 
 namespace ChronoTalk.Views
@@ -20,17 +18,10 @@ namespace ChronoTalk.Views
             vm.Navigation = this.Navigation;
         }
 
-        private async void ListViewOnItemTapped(object sender, ItemTappedEventArgs e)
+        private void ListViewOnItemTapped(object sender, ItemTappedEventArgs e)
         {
             var listview = (ListView)sender;
             listview.SelectedItem = null;
-        }
-
-        private async Task NavigateToSpeakerPage(SpeakerViewModel speaker)
-        {
-            var speakerPage = new SpeakerPage();
-            speakerPage.BindingContext = speaker;
-            await this.Navigation.PushAsync(speakerPage);
         }
     }
 }
